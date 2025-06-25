e9c2d11ef8c4e942de2a718bf4780ed0038911e6
Kyle Tsubaki <kylekaos03@gmail.com>
2025-06-25 10:59:01 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+Project 1: Greeting the User
+Subject: Input/Output to the stream
+
+Instructions: Your objective is to write a program that allows a user to input their name and age and be greeted by the output stream. Further instructions are written as comments in greet.cpp above both Greet() and main(). Anywhere you see "TODO" written is where you should write the implementation.
+
+Example Runs...
+
+Enter your name: Adam
+Enter your age: 19
+Hello, Adam! You are 19 years old.
+
+Enter your name: Kristen
+Enter your age: 22
+Hello, Kristen! You are 22 years old.
---
+++ greet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+// Use this function to output to the stream greeting the user. 
+// Follow this guide: "Hello, [name]! You are [age] years old."
+void Greet(std::string name, int age) {
+    // TODO
+}
+
+// 1. Declare a string variable for the user's name.
+// 2. Declare an integer variable for the user's age.
+// 3. Output to the stream asking for the user's name.
+// 4. Input the user's name to the stream.
+// 5. On a new line, output to the stream asking for the user's age. 
+// 6. Input the user's age to the stream.
+// 7. Call the Greet() function and pass the user's name and age as arguments.
+int main(void) {
+    // TODO
+}
